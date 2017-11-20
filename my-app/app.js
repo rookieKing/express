4f39b8ba4716310e8006ae8f8e0adff94c65c21e
rookie <rookielei@gmail.com>
2017-11-20 11:52:14 +0800

    新增上传文件服务 /server/file-upload

---
+++ my-app/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var multiparty = require('multiparty');
+
+app.use(express.static('../../vue/my-project/dist'));
+
+app.post('/server/file-upload', function (req, res) {
+  //生成multiparty对象，并配置上传目标路径
+  var form = new multiparty.Form({ uploadDir: './uploads/' });
+  form.parse(req, function (err, fields, files) {
+    console.log(err, files);
+    res.send('Hello World!');
+    // don't forget to delete all req.files when done 
+  });
+});
+
+app.get('/test', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
