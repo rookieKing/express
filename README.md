16c1d16737da0ca27ceea9f0376eefd794ebd0ef
rookie <rookielei@gmail.com>
2017-11-21 15:15:05 +0800

    新增测试接口 /server/api1/search

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # express
 
 ```bat
+npm init
 npm install express --save
 npm install multiparty --save
 ```
--- my-app/app.js
+++ my-app/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 var multiparty = require('multiparty');
+var bodyParser = require('body-parser');
 
 app.use(express.static('../../vue/my-project/dist'));
+
+app.use(bodyParser.json());
 
 app.post('/server/file-upload', function (req, res) {
   //生成multiparty对象，并配置上传目标路径
@@ -12,6 +15,15 @@ app.post('/server/file-upload', function (req, res) {
     res.send('Hello World!');
     // don't forget to delete all req.files when done 
   });
+});
+
+app.post('/server/api1/search', (req, res) => {
+  console.log(req.body);
+  res.send([
+    { xx: 11, zz: 21 },
+    { xx: 12, zz: 22 },
+    { xx: 13, zz: 23 },
+  ]);
 });
 
 app.get('/test', function (req, res) {
